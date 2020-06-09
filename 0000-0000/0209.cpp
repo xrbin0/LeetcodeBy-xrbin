630ad63ce7225f27bc5c7e2e76e50597fdb353f2
xrbin0 <xrbin0@163.com>
2020-06-09 22:45:41 +0800

    2020-06-09 22:45:40

--- 0000-0000/0209.cpp
+++ 0000-0000/0209.cpp
@@ -4,14 +4,18 @@ using namespace std;
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
-        int left = 0, right = 0, sum = 0, res = 0x80000000, sizes = nums.size();
-        while(right < sizes){
-            sum += nums[right++];
-            if(sum >= s){
+        if(nums.size() == 0) return 0;
+        int left = 0, right = 0, sum = 0, res = 0x7FFFFFFF, sizes = nums.size();
+        while(left < sizes){
+            // cout << left << " " << right << endl;
+            if(right < sizes) sum += nums[right++];
+            while(left < sizes && sum >= s){
                 res = min(res, right - left);
                 sum -= nums[left++];
             }
+            if(right == sizes && sum < s) break;
         }
+        if(res == 0x7FFFFFFF) res = 0;
         return res;
     }
 };
